b9a125033aac1a8f89ddf161d5ffed71b4307141
hboot <bobolity@163.com>
2024-10-08 20:33:55 +0800

    ci: 编译脚本生成的文件名称

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 在任何命令失败时退出脚本
+set -e  
+
 # 编译环境,当前的系统环境
 os=$1
 
@@ -14,21 +17,24 @@ if [ -f "$BINARY_PATH" ]; then
     echo "Binary build successful: $BINARY_PATH"
     
     # 删除原来的压缩包
-    if ["$os" = "windows-latest"];then
-        rm rsup.zip
-    else 
-        rm rsup.tar.gz
-    fi
+    # if [ "$os" = "windows-latest" ];then
+    #     rm rsup.zip
+    # else 
+    #     rm rsup.tar.gz
+    # fi
+    rm -f rsup-$os.zip rsup-$os.tar.gz
 
     # 创建一个压缩包，将编译后的二进制文件打包到根目录下
-    if ["$os" = "windows-latest"];then
+    if [ "$os" = "windows-latest" ];then
         echo "Compressing binary for windows..."
-        zip -r rsup.zip target/release/rsup.exe
-        echo "Binary successfully compressed to rsup.zip"
+        zip -r rsup-$os.zip target/release/rsup.exe
+
+        echo "Binary successfully compressed to rsup-$os.zip"
     else 
         echo "Compressing binary for $os..."
-        tar -czvf rsup.tar.gz -C target/release rsup
-        echo "Binary successfully compressed to rsup.tar.gz"
+        tar -czvf rsup-$os.tar.gz -C target/release rsup
+
+        echo "Binary successfully compressed to rsup-$os.tar.gz"
     fi
 else
     echo "Error: Binary not found!"
